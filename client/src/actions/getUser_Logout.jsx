4b004562f3e91f3a61882865a961ae12594fd2e7
Thomas Melchers <thomas.melchers@gmail.com>
2022-02-04 11:18:00 +0100

    logout action

---
+++ client/src/actions/getUser_Logout.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const getUser_Logout = () => {
+  axios({
+    method: 'get',
+    url: `${process.env.REACT_APP_API_URL}/api/v1/users/logout`,
+    /* withCredentials: false, */
+  })
+    .then((res) => {localStorage.removeItem('token')
+  console.log('logout')})
+    .catch((err) => console.error(err))
+    window.location = '/'
+}
+
+export default getUser_Logout
