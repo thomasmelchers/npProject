101ef9f3bc1f0f909e6306cbaa4a5245b8179940
Thomas Melchers <thomas.melchers@gmail.com>
2022-02-09 10:21:51 +0100

    modification logout way api

--- client/src/actions/getUser_Logout.jsx
+++ client/src/actions/getUser_Logout.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const getUser_Logout = () => {
   axios({
     method: 'get',
-    url: `${process.env.REACT_APP_API_URL}/api/v1/users/logout`,
+    url: `${process.env.REACT_APP_API_URL}api/v1/users/logout`,
     /* withCredentials: false, */
   })
     .then((res) => {localStorage.removeItem('token')
