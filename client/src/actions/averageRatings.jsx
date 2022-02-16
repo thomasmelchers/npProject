a91c3f6ff66be3204d8a6695dd4c1d39323210ac
Thomas Melchers <thomas.melchers@gmail.com>
2022-02-16 15:27:29 +0100

    rest

---
+++ client/src/actions/averageRatings.jsx
@@ -0,0 +1,8 @@
+const averageRatings = ({ratings}) => {
+ const allRatigns = ratings
+ const nbRatings = ratings.length
+
+
+}
+
+export default averageRatings
---
+++ client/src/pages/ManageMyCottage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddCottage = () => {
+  return <div>THIS IS THE FORM TO ADD A COTTAGE</div>;
+};
+
+export default AddCottage
